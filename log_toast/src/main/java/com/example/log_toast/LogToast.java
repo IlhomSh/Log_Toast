e7cc98f8ad64abf140d4726c40ff804363cacb7f
sh.ilhom <ilhomshergoziyev@gmail.com>
2019-08-19 17:51:03 +0500

    fix

--- log_toast/src/main/java/com/example/log_toast/LogToast.java
+++ log_toast/src/main/java/com/example/log_toast/LogToast.java
@@ -1,11 +1,19 @@
 package com.example.log_toast;
 
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
 
 public class LogToast {
 
     public static void toast(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+
+    public static void newIntent(Context context){
+        Intent intent =new Intent(context,Library_Actvity.class);
+
+        context.startActivity(intent);
     }
 }
